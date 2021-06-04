dc86da06105ad45451cdf84e4f8fceef2515b005
nuts <45058819+nutscreate@users.noreply.github.com>
2021-06-04 16:03:25 +0900

    Load images with file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,13 @@ module.exports = {
                     },
                 ],
             },
+            {
+                test: /\.png|.jpg/,
+                type: 'asset/resource',
+                generator: {
+                    filename: 'images/[name][ext]',
+                },
+            },
         ],
     },
     plugins: [
